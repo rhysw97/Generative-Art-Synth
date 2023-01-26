ea74ed2782fc66ab5ab6d4849e50edc57d3b0131
rhysw97 <rhysw97@gmail.com>
2023-01-26 02:20:25 +0000

    updated UI

--- src/Components/Synth/Sequencer/step/step.tsx
+++ src/Components/Synth/Sequencer/step/step.tsx
@@ -10,15 +10,13 @@ interface StepProps {
 //Some changes I'd make: I'd update Step to remove all of its state and only use the prop for whether its active or not. And you can compute its color based on that prop. So it's completely controlled by the parent 
 export default function Step(props: StepProps) {
     
-    const colour = props.active ? 'purple' : 'white'
+    const colour = props.active ? 'rgb(182, 11, 182)' : 'white'
     
     return (
         <div>
             <div className="step" onClick={() => {
                 props.onClick(!props.active, props.position)   
             }} style={{
-                height: props.height,
-                width: props.width,
                 backgroundColor: colour,
             }}></div>
         </div>
